ce7ed94c3ae189a5b1904079fb2a7dd48de58345
ogoras <skus30@wp.pl>
2021-12-13 18:42:05 +0100

    Note model

--- Core/Domain/Note.cs
+++ Core/Domain/Note.cs
@@ -6,5 +6,12 @@ namespace Core.Domain
 {
     public class Note
     {
+        public int Id { get; set; }
+        public Profile Owner { get; set; }
+        public bool Encrypted { get; set; }
+        public bool? SharedPublically { get; set; }
+        public String Text { get; set; }
+        public List<Profile> ShareRecipients { get; set; }
+        public List<Photo> AttachedPhotos { get; set; }
     }
 }
--- Core/Domain/Photo.cs
+++ Core/Domain/Photo.cs
@@ -7,8 +7,9 @@ namespace Core.Domain
     public class Photo
     {
         public int Id { get; set; }
-        public User Owner { get; set; }
+        public Profile Owner { get; set; }
         public string Url { get; set; }
         public Note? Note { get; set; }
+        public List<Profile> ShareRecipients { get; set; }
     }
 }
