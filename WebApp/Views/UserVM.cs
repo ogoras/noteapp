610d78ae31c7920002cdca464781a2280661a3b6
ogoras <skus30@wp.pl>
2021-12-27 18:06:20 +0100

    created WebApp

---
+++ WebApp/Views/UserVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Views
+{
+    public class UserVM
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime LastLoggedIn { get; set; }
+    }
+}
