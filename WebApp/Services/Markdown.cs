f7a662d2fae183488be6bfb7e307983cfea11c27
ogoras <skus30@wp.pl>
2022-01-22 18:52:06 +0100

    Styling with Markdown or HTML tags, XSS secure

---
+++ WebApp/Services/Markdown.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Html;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Westwind.AspNetCore.Markdown;
+using Westwind.Web.Markdown.MarkdownParser;
+
+namespace WebApp.Services
+{
+    public static class Markdown
+    {
+        public static string Parse(string markdown,
+                        bool usePragmaLines = false,
+                        bool forceReload = false)
+        {
+            if (string.IsNullOrEmpty(markdown))
+                return "";
+
+            var parser = MarkdownParserFactory.GetParser(usePragmaLines, forceReload);
+            return parser.Parse(markdown);
+        }
+
+        public static HtmlString ParseHtmlString(string markdown,
+                        bool usePragmaLines = false,
+                        bool forceReload = false)
+        {
+            return new HtmlString(Parse(markdown, usePragmaLines, forceReload));
+        }
+    }
+}
